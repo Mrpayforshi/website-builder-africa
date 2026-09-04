49b35b3c1a0c77bb0c7037758c8166344d196945
Mrpayforshi <tadiwamandizha3@gmail.com>
2026-09-04 07:47:26 +0200

    Refactor map embed conditionals in Contact component

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -23,8 +23,9 @@ export function Contact({ address, phone, email, hours, map_embed, liteMode = fa
           ))}
         </ul>
       )}
-      {map_embed && !liteMode && <div className="contact__map" dangerouslySetInnerHTML={{ __html: map_embed }} />}
-      {map_embed && liteMode && address && (
+            {map_embed && liteMode && address && (
+        
+          className="contact__map-link"
         
           className="contact__map-link"
           href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`}
